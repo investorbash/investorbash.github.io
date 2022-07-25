11e0cec44f601fedcba6526043aa285ba314da1b
Investor Bash <officialinvestorbash@hotmail.com>
2022-07-25 18:13:35 +0100

    upload-projects

---
+++ netflixmedia.js
@@ -0,0 +1,11 @@
+var a =document.getElementById("inp0").value;
+var b =document.getElementById("span1");
+var c = /^[0-9a-zA-Z]+$/;
+if(a.value.includes(c)){
+b.innerHTML="Valid Input";
+b.style.outline="green";
+}
+else{
+b.innerHTML="Invalid Input";
+b.style.outline="red";
+}
