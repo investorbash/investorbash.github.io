f8c1c6e8bc328d93174a75db0c7f772027f177cb
Investor Bash <officialinvestorbash@hotmail.com>
2022-08-09 21:07:40 +0100

    project-updated

--- netflixmedia.js
+++ netflixmedia.js
@@ -4,8 +4,10 @@ var c = /^[0-9a-zA-Z]+$/;
 if(a.value.includes(c)){
 b.innerHTML="Valid Input";
 b.style.outline="green";
+return true;
 }
 else{
 b.innerHTML="Invalid Input";
 b.style.outline="red";
+return false;
 }
